e7b364fb70707b0cc289cb13bc4afae20193f8cc
rahulgreddy <51025912+rahulgreddy@users.noreply.github.com>
2019-06-04 09:55:39 -0400

    Update java.java

--- java.java
+++ java.java
@@ -2,8 +2,8 @@ public class java
 {
 public static void main(String[] args) 
 {
-int a=5, b=10, c=10, d=10, sum;
-sum=a + b + c + d;
+int a=5, b=10, c=10, d=10, e=10, sum;
+sum=a + b + c + d + e;
 System.out.println(" sum of two numbers: "+sum);
 }
 }
